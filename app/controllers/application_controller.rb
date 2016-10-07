74b58dbff2c3127c4cce8dbf3ff191b603fac759
Franciszek Milczarski <franko.gen@gmail.com>
2016-10-07 13:44:51 +0000

    Add Category One, Two, Three to page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  
+    before_filter :getCategoryNav
+    
+  
   protect_from_forgery with: :exception
+  
+  private
+  def getCategoryNav
+      @categoryNav = Category.all
+      
+  end
 end
